3e66879a8b7f8796c44e0c113ce6da248b159161
362579823 <362579823@2260I-L29-030>
2026-09-08 13:18:57 -0400

    Add project files.

---
+++ Part 1 Basics/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+string favMovie, firstName;
+Console.WriteLine("FavMovie ");
+Console.WriteLine("///////////////////////////////////////////");
+Console.WriteLine("press enter to continue");
+Console.ReadLine();
+Console.WriteLine("Please enter your first name:");
+firstName = Console.ReadLine();
+Console.WriteLine("Hi" + firstName);
