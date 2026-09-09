90d60667f575497f87075ae4ce2203e4c93dabee
melod <melod@ANDERSON>
2026-09-09 11:35:01 -0400

    0,2

--- Part 1 Basics/Program.cs
+++ Part 1 Basics/Program.cs
@@ -1,10 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.ForegroundColor = ConsoleColor.DarkGreen;
 string favMovie, firstName;
-Console.WriteLine("FavMovie ");
+Console.WriteLine("FavMovie.com");
 Console.WriteLine("///////////////////////////////////////////");
 Console.WriteLine("press enter to continue");
+
 Console.ReadLine();
+Thread.Sleep(1000);
+Console.Clear();
 Console.WriteLine("Please enter your first name:");
 firstName = Console.ReadLine();
-Console.WriteLine("Hi" + firstName);
+Console.WriteLine("Hi " + firstName.ToLower());
+Console.WriteLine("press enter to continue");
+Console.ReadLine();
+Console.Clear();
+Thread.Sleep(1000);
+Console.WriteLine("Please enter your favorite movie:");
+favMovie = Console.ReadLine();
+Console.WriteLine("press enter to continue");
+Console.ReadLine();
+Console.Clear();
+Thread.Sleep(1000);
+Console.WriteLine("Welcome to FavMovie.com " + firstName.ToLower() + "!");
+Console.WriteLine("////////////////////////////////////////////////////////");
